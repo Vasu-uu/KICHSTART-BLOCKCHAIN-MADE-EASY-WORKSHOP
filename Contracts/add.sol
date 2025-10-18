93cd6e77a34388eb5522d90697b3ada5f721e7eb
Vasu-uu <vasudev20057d@gmail.com>
2025-10-18 06:58:45 +0100

    Added Contracts and PPT

---
+++ Contracts/add.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.20;
+
+contract Sum {
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+}
